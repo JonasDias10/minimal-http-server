1c3fbb358a661e926d11dc7fd951cc8e78d7a36c
Jonas Dias <zdias1413@gmail.com>
2024-05-25 00:19:55 -0300

    feat: improve client handler

--- minimal-http-server/src/server/client.rs
+++ minimal-http-server/src/server/client.rs
@@ -1,4 +1,5 @@
 use super::request::Request;
+use super::response::{self};
 use super::storage;
 use std::io::{Result, Write};
 use std::net::TcpStream;
@@ -14,47 +15,18 @@ pub fn handle_client(mut stream: TcpStream) -> Result<()> {
     };
 
     match request.method.as_str() {
-        "GET" => match storage::get_file(&request.path) {
-            Ok(buffer) => {
-                let content_type = match request.path.split('.').last() {
-                    Some(extension) => match extension {
-                        "html" => "text/html",
-                        "css" => "text/css",
-                        "js" => "application/javascript",
-                        "json" => "application/json",
-                        "svg" => "image/svg+xml",
-                        "png" => "image/png",
-                        "jpg" | "jpeg" => "image/jpeg",
-                        _ => "text/plain",
-                    },
-                    None => "text/plain",
-                };
+        "GET" => {
+            let response = match response::Response::create_response(&request) {
+                Ok(response) => response,
+                Err(err) => {
+                    println!("Failed to create response: {}", err);
+                    return Ok(());
+                }
+            };
 
-                let headers = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
-                    content_type,
-                    buffer.len()
-                );
-
-                let body = String::from_utf8_lossy(&buffer[..]);
-
-                let response = format!("{}{}", headers, body);
-
-                stream
-                    .write(response.as_bytes())
-                    .expect("Failed to write to stream");
-
-                stream.flush().expect("Failed to flush stream");
-
-                println!("File downloaded successfully!");
-            }
-            Err(exception) => {
-                println!("Error: {}", exception);
-
-                let content = format!("Error: {}", exception).into_bytes();
-                stream.write(&content).expect("Failed to write to stream");
-            }
-        },
+            let response = response.as_bytes();
+            stream.write(&response).expect("Failed to write to stream");
+        }
         "POST" => match storage::save_file(&request.path, &request.body) {
             Ok(()) => {
                 println!("File saved successfully!");
