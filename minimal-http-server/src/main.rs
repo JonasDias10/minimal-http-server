0564e6c1a81dd1b9e1d81a364d9a4d9bff8c6c28
Jonas Dias <zdias1413@gmail.com>
2024-05-26 10:37:24 -0300

    feat: improve error handling

--- minimal-http-server/src/main.rs
+++ minimal-http-server/src/main.rs
@@ -5,12 +5,16 @@ use std::io::Result;
 use std::net::TcpListener;
 
 fn main() -> Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080")
+        .unwrap_or_else(|error| panic!("Failed to bind to port 8080: {}", error));
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => client::handle_client(stream).unwrap(),
-            Err(error) => println!("Error: {}", error),
+            Ok(stream) => match client::handle_client(stream) {
+                Ok(()) => (),
+                Err(error) => println!("Error when handling client: {}", error),
+            },
+            Err(error) => println!("Error when creating stream: {}", error),
         }
     }
 
